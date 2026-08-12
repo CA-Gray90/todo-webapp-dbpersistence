51d24d14b3a2f7d2afa4d59e13b62dc1b270a787
Charles <cgray888@gmail.com>
2026-08-12 20:23:52 +1000

    Optimized SQL queries to reduce number of additional queries (n+1)

--- todos/database_persistence.py
+++ todos/database_persistence.py
@@ -58,7 +58,7 @@ class DatabasePersistence:
                         );
                     """)
 
-    def _find_todos_for_list(self, list_id):
+    def find_todos_from_list(self, list_id):
         query = 'SELECT * FROM todos WHERE list_id = %s'
         logger.info("Executing query: %s with list_id: %s", query, list_id)
 
@@ -70,7 +70,17 @@ class DatabasePersistence:
         return results
 
     def find_list(self, list_id):
-        query = 'SELECT * FROM lists WHERE id = %s'
+        query = '''
+        SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.completed, True)) AS todos_remaining
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+        WHERE lists.id = %s
+        GROUP BY lists.id
+        ORDER BY lists.title;
+        '''
+
         logger.info("Executing query: %s with list_id: %s", query, list_id)
 
         with self._database_connect() as connection:
@@ -78,12 +88,19 @@ class DatabasePersistence:
                 cursor.execute(query, (list_id,))
                 results = dict(cursor.fetchone())
 
-        todos = self._find_todos_for_list(list_id)
-        results.setdefault('todos', todos)
         return results
 
     def all_lists(self):
-        query = 'SELECT * FROM lists'
+        query = '''
+        SELECT lists.*,
+        COUNT(todos.id) AS todos_count,
+        COUNT(NULLIF(todos.completed, True)) AS todos_remaining
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+        GROUP BY lists.id
+        ORDER BY lists.title;
+        '''
+
         logger.info("Executing query: %s", query)
 
         with self._database_connect() as connection:
@@ -92,10 +109,6 @@ class DatabasePersistence:
                 results = cursor.fetchall()
 
         lists = [dict(result) for result in results]
-        for lst in lists:
-            todos = self._find_todos_for_list(lst['id'])
-            lst.setdefault('todos', todos)
-        
         return lists
 
     def create_new_list(self, title):
